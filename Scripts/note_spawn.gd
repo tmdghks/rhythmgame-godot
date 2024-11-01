9840965d64bfacffd1049b0776d2d775a1a43ab1
SeungHwan Kim <tmdghks12@korea.ac.kr>
2024-11-02 00:12:09 +0900

    wip: note spawn logic
    mod: scale of note

--- Scripts/note_spawn.gd
+++ Scripts/note_spawn.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var note_scene: PackedScene  # Note.tscn 파일을 인스펙터에서 할당할 수 있도록 설정
+var spawn_interval = 1.0  # 노트 생성 간격 (초 단위)
+var spawn_timer = 0.0  # 타이머 변수
 
+func _process(delta):
+	# 매 프레임마다 타이머를 줄이고, 0 이하가 되면 노트를 생성
+	spawn_timer -= delta
+	if spawn_timer <= 0:
+		spawn_note()
+		spawn_timer = spawn_interval  # 타이머 초기화
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func spawn_note():
+	# note_scene이 설정되어 있으면 노트를 인스턴스화하고 메인 씬에 추가
+	if note_scene:
+		var note_instance = note_scene.instantiate()
+		note_instance.position = Vector2(100, 100)  # 노트가 나타날 위치
+		add_child(note_instance)
